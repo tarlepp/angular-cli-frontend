aa9c72a7df1676d7861eaad7c267829218b41ba2
tarlepp <tarmo.leppanen@protacon.com>
2016-10-30 01:16:06 +0300

    Refactored login component.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,8 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, Input, ViewChild, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MdInput } from '@angular/material';
+
+import { AuthService, UserService } from '../services/';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +11,69 @@ import { Component, ViewEncapsulation } from '@angular/core';
   encapsulation: ViewEncapsulation.None,
 })
 
-export class LoginComponent {
-  public username: string = '';
-  public password: string = '';
+export class LoginComponent implements OnInit {
+  @ViewChild('usernameControl')
+  usernameControl: MdInput;
 
-  constructor() { }
+  @ViewChild('passwordControl')
+  passwordControl: MdInput;
 
-  doLogin(event: Event) {
-    console.log('TODO implement actual login');
-    console.log(this.username, this.password, event);
+  @Input()
+  username: string;
+
+  @Input()
+  password: string;
+
+  /**
+   * Constructor of the class.
+   *
+   * @param {AuthService}         authService
+   * @param {UserService}         userService
+   * @param {Router}              router
+   */
+  constructor(
+    private authService: AuthService,
+    private userService: UserService,
+    private router: Router
+  ) { }
+
+  /**
+   * On component init we want to set focus to username / email input.
+   */
+  ngOnInit(): void {
+    this.usernameControl.focus();
+  }
+
+  /**
+   * Method to make actual login
+   *
+   * @param {Event} event
+   */
+  public login(event: Event) {
+    event.stopPropagation();
+    event.preventDefault();
+
+    this.authService
+      .login({username: this.username, password: this.password})
+      .subscribe(
+        (data) => {
+          // Store tokens for current user
+          this.userService.storeTokens(data);
+
+          // And redirect user to profile page
+          this.router.navigate(['auth/profile']);
+        },
+        (error) => {
+          console.log('Login error',  error);
+
+          // Clear local storage data
+          this.userService.erase();
+
+          // Reset form data
+          this.username = '';
+          this.password = '';
+        }
+      )
+    ;
   }
 }
