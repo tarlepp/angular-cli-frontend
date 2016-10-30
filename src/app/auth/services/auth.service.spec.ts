94fa78bb52e3dbcc10dff9c5636693ee78a3c75c
tarlepp <tarmo.leppanen@protacon.com>
2016-10-30 16:24:00 +0200

    Fixed AuthService base tests.

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -1,16 +1,43 @@
-/* tslint:disable:no-unused-variable */
+import { TestBed, inject } from '@angular/core/testing';
+import { Http, ConnectionBackend, HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { AuthHttp, provideAuth, JwtHelper } from 'angular2-jwt';
+import { LocalStorageService } from 'ng2-webstorage';
 
-import { TestBed, async, inject } from '@angular/core/testing';
-import { AuthService } from './auth.service';
+import { AuthService, UserService } from './';
 
-describe('Service: Auth', () => {
+describe('Service: /src/app/auth/services/auth.service.ts', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [AuthService]
+      imports: [
+        HttpModule,
+        RouterModule.forRoot([]),
+      ],
+      providers: [
+        AuthService,
+        UserService,
+        Http,
+        AuthHttp,
+        ConnectionBackend,
+        JwtHelper,
+        LocalStorageService,
+        provideAuth({
+          tokenGetter: (() => {
+            let storage = new LocalStorageService();
+
+            return storage.retrieve('token');
+          }),
+        }),
+        {
+          provide: APP_BASE_HREF,
+          useValue : '/',
+        },
+      ],
     });
   });
 
-  it('should ...', inject([AuthService], (service: AuthService) => {
+  it('should create service', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
   }));
 });
