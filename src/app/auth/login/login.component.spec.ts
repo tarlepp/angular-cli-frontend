3501af8666045cd2aaa47f226fc0180f28488e3b
tarlepp <tarmo.leppanen@protacon.com>
2016-10-30 15:36:57 +0200

    Fixed LoginComponent base tests.

--- src/app/auth/login/login.component.spec.ts
+++ src/app/auth/login/login.component.spec.ts
@@ -1,8 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule } from '@angular/forms';
-import { MaterialModule } from '@angular/material';
+import { APP_BASE_HREF } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { provideAuth, JwtHelper } from 'angular2-jwt';
+import { LocalStorageService } from 'ng2-webstorage';
 
 import { LoginComponent } from './login.component';
+import { AuthService, UserService } from '../services/';
+import { SharedModule } from '../../shared/shared.module';
 
 describe('Component: /auth/login/login.component.ts', () => {
   let component: LoginComponent;
@@ -14,9 +18,25 @@ describe('Component: /auth/login/login.component.ts', () => {
         LoginComponent,
       ],
       imports: [
-        FormsModule,
-        MaterialModule.forRoot(),
+        SharedModule,
+        RouterModule.forRoot([]),
       ],
+      providers: [
+        AuthService,
+        UserService,
+        JwtHelper,
+        provideAuth({
+          tokenGetter: (() => {
+            let storage = new LocalStorageService();
+
+            return storage.retrieve('token');
+          }),
+        }),
+        {
+          provide: APP_BASE_HREF,
+          useValue : '/',
+        },
+      ]
     })
     .compileComponents();
   }));
