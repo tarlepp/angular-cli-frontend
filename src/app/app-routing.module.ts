690b189e0ca5394cb51ea874eef40fd8358c6ee4
tarlepp <tarmo.leppanen@protacon.com>
2017-02-22 20:06:21 +0200

    refactor: app routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
-import { AboutRoutes } from './about/';
-import { AuthRoutes } from './auth/';
-import { LayoutRoutes } from './layout/';
-
 const appRoutes = [
   {
     path: '',
     pathMatch: 'full',
     redirectTo: 'about',
   },
-  ...AboutRoutes,
-  ...AuthRoutes,
-  ...LayoutRoutes,
 ];
 
 @NgModule({
