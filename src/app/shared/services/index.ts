25d9de16ff2c704b3ebee60d58ece5e3da73b4ff
tarlepp <tarmo.leppanen@protacon.com>
2017-03-21 23:39:52 +0200

    feat: Added translation service.

--- src/app/shared/services/index.ts
+++ src/app/shared/services/index.ts
@@ -1,10 +1,13 @@
 import { MessageService } from './message.service';
 import { ConfigService } from './config.service';
+import { TranslationService } from './translation.service';
 
-export * from './message.service';
 export * from './config.service';
+export * from './message.service';
+export * from './translation.service';
 
 export const Services = [
-  MessageService,
   ConfigService,
+  MessageService,
+  TranslationService
 ];
---
+++ src/app/shared/services/translation.service.ts
@@ -0,0 +1,53 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { TranslateService } from '@ngx-translate/core';
+import { Observable } from 'rxjs/Observable';
+
+import { ConfigService } from './config.service';
+
+@Injectable()
+export class TranslationService {
+  private translationUrl: string;
+
+  /**
+   * Constructor of the class.
+   *
+   * @param {Http}              http
+   * @param {TranslateService}  translateService
+   * @param {ConfigService}     configService
+   */
+  public constructor(
+    private http: Http,
+    private translateService: TranslateService,
+    private configService: ConfigService
+  ) {
+    // Determine what url to use to fetch these translations; 1) local 2) remote
+    this.translationUrl = this.configService.get('USE_LOCAL_TRANSLATIONS')
+      ? `./assets/i18n/`
+      : `${this.configService.getApiUrl()}translation/`
+    ;
+  }
+
+  /**
+   * Getter method for domain specified translations. Note that this method fill load these translations automatically
+   * to translate service so that those are usable right away.
+   *
+   * @param {string}  domain
+   * @returns {Observable<Object>}
+   */
+  public loadTranslationsForDomain(domain: string): Observable<Object> {
+    return this.http
+      .get(`${this.translationUrl}${domain}/${this.translateService.currentLang}.json`)
+      .map((res: Response) => { // Aah, happy path - so happy now
+        // Merge translations to current one
+        this.translateService.setTranslation(this.translateService.currentLang, res.json(), true);
+
+        return res.json();
+      })
+      .catch((error: any) => { // And in any error we just want to resolve true and log possible errors...
+        console.error('Translation not found...', error);
+
+        return Observable.of({});
+      });
+  }
+}
