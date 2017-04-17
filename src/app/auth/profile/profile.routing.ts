10a765b5ef83c0b67884102be455d86d29d2e274
tarlepp <tarmo.leppanen@protacon.com>
2017-04-17 12:32:48 +0300

    Implemented localization for auth/profile view.

--- src/app/auth/profile/profile.routing.ts
+++ src/app/auth/profile/profile.routing.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 
 import { ProfileComponent } from './profile.component';
 import { AuthenticationGuard } from '../guards/';
+import { TranslationGuard } from '../../shared/translation/';
 import { ProfileLocalResolver, ProfileRemoteResolver } from './resolves/';
 
 export const ProfileRoutes: Routes = [
@@ -10,6 +11,7 @@ export const ProfileRoutes: Routes = [
     component: ProfileComponent,
     canActivate: [
       AuthenticationGuard,
+      TranslationGuard
     ],
     resolve: {
       profileLocal: ProfileLocalResolver,
