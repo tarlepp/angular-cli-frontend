71a2f771c28f6fe205a958a2d76bec949695d596
tarlepp <tarmo.leppanen@protacon.com>
2017-04-18 20:40:27 +0300

    Added 'Routes' type

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
-const appRoutes = [
+const appRoutes: Routes = [
   {
     path: '',
     pathMatch: 'full',
